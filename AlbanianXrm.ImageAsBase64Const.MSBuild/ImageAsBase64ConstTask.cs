6e7d04e2201bf953aa5184cf6846bb963123ce0f
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2025-11-15 10:09:21 +0100

    feat: First release

---
+++ AlbanianXrm.ImageAsBase64Const.MSBuild/ImageAsBase64ConstTask.cs
@@ -0,0 +1,72 @@
+﻿/*
+   Copyright 2025 AlbanianXrm <albanian@xrm.al>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace AlbanianXrm
+{
+    public class ImageAsBase64ConstTask : Task
+    {
+        [Required]
+        public string InputFile { get; set; }
+
+        public string Namespace { get; set; }
+
+        [Required]
+        public string ClassName { get; set; }
+
+        [Required]
+        public string FieldName { get; set; }
+
+        [Output]
+        public string OutputFile { get; set; }
+
+        private static readonly ToBase64Transform base64Transform = new ToBase64Transform();
+        public override bool Execute()
+        {
+            FileInfo fileInfo = new FileInfo(InputFile);
+            if (fileInfo.Exists == false)
+            {
+                Log.LogError($"Input file '{InputFile}' does not exist.");
+                return false;
+            }
+            using (var reader = new StreamReader(new CryptoStream(fileInfo.OpenRead(), base64Transform, CryptoStreamMode.Read)))
+            {
+                OutputFile = InputFile + ".g.cs";
+                using (var writer = new StreamWriter(OutputFile, false))
+                {
+                    if (!string.IsNullOrWhiteSpace(Namespace))
+                    {
+                        writer.WriteLine($"namespace {Namespace}");
+                        writer.WriteLine("{");
+                    }
+                    writer.WriteLine($"    public static partial class {ClassName}");
+                    writer.WriteLine("    {");
+                    writer.WriteLine($"        public const string {FieldName} = \"{reader.ReadToEnd()}\";");
+                    writer.WriteLine("    }");
+                    if (!string.IsNullOrWhiteSpace(Namespace))
+                    {
+                        writer.WriteLine("}");
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# ImageAsBase64Const-Generator
-This is a dotnet source generator that converts an image to a base64 string const. Useful for XrmToolBox tool icons.
+> Copyright 2025 AlbanianXrm <albanian@xrm.al>
+> 
+> Licensed under the Apache License, Version 2.0 (the "License");
+> you may not use this file except in compliance with the License.
+> You may obtain a copy of the License at
+> 
+>     http://www.apache.org/licenses/LICENSE-2.0
+> 
+> Unless required by applicable law or agreed to in writing, software
+> distributed under the License is distributed on an "AS IS" BASIS,
+> WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+> See the License for the specific language governing permissions and
+> limitations under the License.
+
+# AlbanianXrm.ImageAsBase64ConstGenerator.MSBuild
+
+This MSBuild package can convert the referenced Image files to Base64 string constants in your application. You need to specify the details of the generated code in a `None` item and it will be picked up by the custom task.
+Simple usage example:
+
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<Project>
+	<ItemGroup>
+		<None Update="Resources\ShkoOnline32x32.png">
+			<Namespace>ShkoOnline.DataverseExcelReporter</Namespace>
+			<ClassName>Resources</ClassName>
+			<ImageAsBase64Const>ShkoOnline32x32</ImageAsBase64Const>
+		</None>
+	</ItemGroup>
+	<ItemGroup>
+		<PackageReference Include="AlbanianXrm.ImageAsBase64ConstGenerator.MSBuild" Version="1.0.0" PrivateAssets="All" />
+	</ItemGroup>
+</Project>
+```
+
+The previous example will generate a class named `Resources` in the `ShkoOnline.DataverseExcelReporter` namespace with a constant string property named `ShkoOnline32x32` containing the Base64 representation of the image.
